ac9b3a26bd49bd0ba9353faf009db053210f427a
Bruce Gordon <bruce@focusls.com>
2019-02-23 14:10:25 -0500

    1

---
+++ PrepSite/Models/Media/GenericFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace PrepSite.Models.Media
+{
+    [ContentType(DisplayName = "GenericFile", GUID = "c903157f-6b81-4eea-80aa-b0dd033df8e5", Description = "")]
+    /*[MediaDescriptor(ExtensionString = "pdf,doc,docx")]*/
+    public class GenericFile : MediaData
+    {
+        /*
+                [CultureSpecific]
+                [Editable(true)]
+                [Display(
+                    Name = "Description",
+                    Description = "Description field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string Description { get; set; }
+         */
+    }
+}
---
+++ PrepSite/Models/Media/ImageFile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+
+namespace PrepSite.Models.Media
+{
+    [ContentType(DisplayName = "ImageFile", GUID = "4d1039cb-0511-4910-97d1-7970164a73fe", Description = "")]
+    [MediaDescriptor(ExtensionString = "png,gif,jpg,jpeg")]
+    public class ImageFile : ImageData
+    {
+
+        [CultureSpecific]
+        [Editable(true)]
+        public virtual string Description { get; set; }
+
+    }
+}
